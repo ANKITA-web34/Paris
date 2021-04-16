945688a1f561f0d1fa0d9677f0928b88705dbedb
ANKITA-web34 <anki333soul@gmail.com>
2021-04-16 20:09:44 +0530

    preLoading code

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
+let preLoading = document.getElementById('loading');
+
+function myFunction() {
+    preLoading.style.display = 'none';
+}
+
 
 let icon = document.getElementById("icon");
 icon.onclick = function() {
     document.body.classList.toggle("dark-theme");
-    if(document.body.classList.contains("dark-theme")) {
+    if(document.body.classList.contains("dark-theme")){
         icon.src = "./img/sun.png";
+
     } else {
-        icon.src = "./img/moon.png";
+        icon.src = "./img/moon.png"
+
     }
 }
